3b7108e85fda51e43b10cd5ce6fae341515394a3
MChen <mchen2016@gmail.com>
2014-04-22 16:57:05 -0400

    add head file for LIS

---
+++ C_ver/include/dynmcprgrm/LongestIncreaseSubsequence.h
@@ -0,0 +1,13 @@
+/**********************************************************************
+ * File: LongestIncreaseSubsequence.h
+ * Author: M Chen (mchen2016@gmail.com)
+ *
+ * An implementation of the longest increasing subsequence problem. 
+ * There are two approaches to solve this problem as far as I know:
+ * one is via the dynamic programming, it runs at O(n^2) time; the 
+ * other is a customize approach, runs at O(nlogn) time. I first 
+ * would like to implement the dp approach.
+ **********************************************************************
+ */
+
+void solve_lngstIncrsSubsqnc_dynmcprgrm();
